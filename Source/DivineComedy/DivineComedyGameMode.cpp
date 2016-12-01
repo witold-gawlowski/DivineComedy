d6e6529e752ad981d5865ef206bc7c7d28663619
witold-gawlowski <witek.gawlowski@gmail.com>
2016-12-01 22:54:44 +0000

    Unreal FPS C++ project

---
+++ Source/DivineComedy/DivineComedyGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "DivineComedy.h"
+#include "DivineComedyGameMode.h"
+#include "DivineComedyHUD.h"
+#include "DivineComedyCharacter.h"
+
+ADivineComedyGameMode::ADivineComedyGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ADivineComedyHUD::StaticClass();
+}
